519bdc737b9d8776ee1fca552e1e339e362b2154
LLong <l916617054@163.com>
2016-12-07 10:17:33 +0800

    添加案例1

--- python/test1.py
+++ python/test1.py
@@ -2,3 +2,7 @@
 # 输入和输出
 print 500 +  400;
 print "sssss";
+print '''line1
+line2
+line3''';
+print '''line1 line2 lin3''';
---
+++ python/test2.py
@@ -0,0 +1,10 @@
+import os;
+
+#对当前文件夹的所有文件进行遍历，得到去掉文件名的后2位的 文件名字符串并写入到names.txt文本中
+filenames = os.listdir(os.getcwd());
+for name in filenames:
+	filenames[filenames.index(name)] = name[:-2];
+out = open('names.txt','w');
+for name in filenames:
+	out.write(name+'\n');
+out.close();
