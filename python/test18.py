09d39ef01ba956aec521dea7475e647fd9bd2ec5
LLong <l916617054@163.com>
2017-05-09 20:31:25 +0800

    字符编码

--- python/test18.py
+++ python/test18.py
@@ -1,3 +1,19 @@
 #encoding=utf-8;
 
 import os;
+import sys;
+import subprocess;
+
+# print sys.argv;#得到当前python文件的文件名和所有参数值
+os.system('cd /Users/sangfor/Desktop/appleSample-master')
+# if __name__ == '__main__':
+#     # print 'Program name',sys.argv[0]; #当前python文件的文件名
+#
+#     index = int(sys.argv[1]);
+#     if(index == 0):
+#         var = {
+#             # subprocess.call('cd /')
+#             # print('fdsfs')
+#
+#         }
+
