bf871d6886758c013f3f3cdc4ddaeef0cf87f0c4
LLong <l916617054@163.com>
2017-05-16 23:50:33 +0800

    win第一次python

--- python/test22.py
+++ python/test22.py
@@ -1,3 +1,17 @@
 #encoding=utf-8;
 
 import os;
+
+s = 0
+def add(a,b,c,d):
+    global s
+    s1 = 0
+    print (len(d) <=1 and d or 2)
+    for i in range(a , b):
+        if(c <= 0):
+            c = 1
+        s += (i**c+sum(d))
+
+
+add(1,10,1,[1])
+print (s)
