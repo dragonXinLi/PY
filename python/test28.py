8a9ef865039a1c2d7dd2f4ac98a51bbae352becd
LLong <l916617054@163.com>
2017-08-09 00:46:52 +0800

    py3-28

--- python/test28.py
+++ python/test28.py
@@ -1,3 +1,20 @@
 #encoding=utf-8;
 
 import os;
+import time,threading
+
+def loop():
+    print("prite %s is running..." %threading.current_thread().name)
+    n = 0
+    while n <5:
+        n = n+1
+        print("thread %s >>> %s" % (threading.current_thread().name , n))
+        time.sleep(1)
+    print("thread %s ended " %threading.current_thread().name)
+
+print("thread %s is running..." % threading.current_thread().name)
+# t = threading.Thread(target=loop(), name= 'LoopThread') loop()直接执行了,
+t = threading.Thread(target=loop, name= 'LoopThread')
+t.start()
+t.join()
+print("thread %s ended" %threading.current_thread().name)
