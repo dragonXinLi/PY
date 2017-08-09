78098bc8311b25fbb9b11d10d066aebb9e36a979
LLong <l916617054@163.com>
2017-08-09 23:55:08 +0800

    py3-base64

--- python/test34.py
+++ python/test34.py
@@ -1,3 +1,20 @@
 #encoding=utf-8;
 
 import os;
+import base64
+
+def safe_base64_decode(s):
+    b = len(s)%4
+    if b!=0:
+        return base64.b64decode(s+b'='*(len(s)%4))
+    else:
+        return base64.b64decode(s)
+    # if len(s) % 4 != 0:
+        # a = b'='*(len(s) % 4)
+        # s += a
+        # # return base64.b64decode(s+b'='*(len(s) % 4))
+        # # return base64.b64decode(s+b'='*(len(s)%4))
+        # print(a)
+
+
+print(safe_base64_decode('YWJjZA'))
