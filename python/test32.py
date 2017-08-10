7ead46141c3266314229ac1f108e6eb00beed843
LLong <l916617054@163.com>
2017-08-10 21:06:12 +0800

    md5

--- python/test32.py
+++ python/test32.py
@@ -1,3 +1,34 @@
 #encoding=utf-8;
 
 import os;
+import struct
+
+#struct
+
+# 准确地讲，Python没有专门处理字节的数据类型。但由于b'str'可以表示字节，所以，字节数组＝二进制str
+
+
+print(struct.pack('>I' , 10240099))
+
+# pack的第一个参数是处理指令，'>I'的意思是：>表示字节顺序是big-endian，也就是网络序，I表示4字节无符号整数。
+
+
+def is_bmp(d):
+    if not os.path.isfile(d):
+        print('The file path does not exist.')
+        return
+    try:
+        with open(d , 'rb') as f:
+            bitOfBmp = f.read()
+            if(len(bitOfBmp) < 30):
+                print('The file is not bitmap.')
+            else:
+                fileInfo = struct.unpack('<ccIIIIIHH' , bitOfBmp)
+                if(fileInfo[0] == b'B' and fileInfo[1] == b'M'):
+                    print('the size of BitMap is %s * %s , #color is %s' %fileInfo[6] %fileInfo[7] %fileInfo[9])
+    except:
+        print('not bitmap')
+        return
+
+d = input('the file path:')
+is_bmp(d)
--- python/test33.py
+++ python/test33.py
@@ -1,3 +1,17 @@
 #encoding=utf-8;
 
 import os;
+import hashlib
+
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+
+
+def calc_md5(password):
+    md5 = hashlib.md5
+    md5.update(password.encode('uft-8'))
+    pwd_md5 = md5.hexdigest()
+    return pwd_md5
