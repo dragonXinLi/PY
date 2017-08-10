a8fce5749b6ffc500306ab6357f0bdb24f3827d2
LLong <l916617054@163.com>
2017-08-10 23:42:25 +0800

    py3-33：MD5-用户输入账号密码，MD5对密码加密后再对比数据库的加密密码

--- python/test33.py
+++ python/test33.py
@@ -3,6 +3,7 @@
 import os;
 import hashlib
 
+# Python的hashlib提供了常见的摘要算法，如MD5，SHA1等等。
 db = {
     'michael': 'e10adc3949ba59abbe56e057f20f883e',
     'bob': '878ef96e86145580c38c87f0410ad153',
@@ -11,7 +12,22 @@ db = {
 
 
 def calc_md5(password):
-    md5 = hashlib.md5
-    md5.update(password.encode('uft-8'))
+    md5 = hashlib.md5()
+    md5.update(password.encode('utf-8'))
     pwd_md5 = md5.hexdigest()
     return pwd_md5
+
+def login(user,password):
+    if user in db:
+        print(password)
+        if db[user] == calc_md5(password):
+            print('welcome')
+            return True
+        else:
+            print('密码错误')
+            return False
+
+user = str(input('请输入你的用户名:'))
+password = str(input('请输入你的密码:'))
+login(user,password)
+
